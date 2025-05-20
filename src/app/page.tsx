a50f4474ce7e86cfa6bb1d651473aa1240294dcd
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2025-05-20 12:32:41 -0700

    page.tsx forgotten in last commit -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <TattooHero />
       <HorizontalSplitParallax />
       <ArtistShowcase />
-      <ParallaxSections />
+      <ParallaxSections /> 
       <DynamicGallery />
       <CtaSection />
     </div>
